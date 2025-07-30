72f31f6d4b55d1f39ec2f2d2fa4cb4af589571f4
gurdian0614 <ivan.gurdian@outlook.com>
2025-07-29 20:41:33 -0600

    Destructure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// Desestructuracion
+const persona = {
+    nombre: 'Roger',
+    apellido: 'Gurdian',
+    edad: 15,
+    direccion: {
+        ciudad: 'SPS',
+        lng: 34.923321,
+        lat: 14.3232
+    }
+};
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const {nombre, edad, direccion} = persona;
+console.log(nombre);
+console.log(edad);
+console.log(direccion.ciudad);
+
+const arreglo = ['1', 2, 3, 4];
+const [arr1, arr2, arr3, arr4] = arreglo;
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+console.log(arr4);
